07d42998d2bfaa2afb7ac13db1d224653e665738
Artur Wronowski <arteqw@gmal.com>
2014-11-07 18:04:10 +0100

    zmiana wysokosci glowicy po zmianie szyby na 6mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -439,8 +439,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 30
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 5
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -13.6//
-  // -13.6 dla PLA
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -11.81
 
   #define Z_RAISE_BEFORE_HOMING 20       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
